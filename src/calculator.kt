2f9e6ce4670f090c4df4d30b9248b91442e16d53
zernemu-boop <zernemu@gmail.com>
2026-07-15 09:45:42 +0300

    update

--- src/calculator.kt
+++ src/calculator.kt
@@ -1,1 +1,22 @@
 //A simple calculator program using a when statement
+fun main() {
+
+    print("Enter first number: ")
+    var num1 = readln().toDouble()
+
+    print("Enter second number: ")
+    var num2 = readln().toDouble()
+
+    print("Enter operator (+, -, *, /): ")
+    var operator = readln()
+
+    var answer = when (operator) {
+        "+" -> num1 + num2
+        "-" -> num1 - num2
+        "*" -> num1 * num2
+        "/" -> num1 / num2
+        else -> "Invalid Operator"
+    }
+
+    println("Answer = $answer")
+}
